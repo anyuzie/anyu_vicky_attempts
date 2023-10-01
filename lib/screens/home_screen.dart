3f6614823c9ec42d2d547650a9526e2e1056b7ca
an yu <aannyuuleong@gmail.com>
2023-10-02 03:04:54 +0800

    added post!

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -48,11 +48,53 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
               ),
               const SizedBox(height: 20.0), // Add spacing between search bar and posts
-              ListView(
-                children: [
-                  
-                ]
-              )
+              Row(
+                children: <Widget>[
+  // User Image (on the left) with padding
+              Padding(
+                padding: EdgeInsets.only(right: 8.0), // Adjust the padding as needed
+                child: CircleAvatar(
+                  backgroundImage: AssetImage('assets/images/avatars/30.png'),
+                  radius: 20.0, // Adjust the radius as needed
+                ),
+              ),
+
+              // Title, Username, and Caption (in a column)
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    // Username
+                    Text(
+                      "handymandy",
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+
+                    // Title
+                    Text(
+                      "First time caregiver tips",
+                      style: TextStyle(fontSize: 24.0),
+                    ),
+
+                    // Caption
+                    Text(
+                    "I’m overwhelmed because I feel like I’m doing everything wrong. My main priority is to make sure my mother is totally comfortable and pain-free while she is in her transition phase. I feel guilty when she’s sleeping comfortably and I have to move her to get her cleaned up or shift her position to prevent sores, and she weeps in pain. It’s the toughest part about all this. I love caring for her because I love her, I’m so busy with things that I have become somewhat disassociated (I still cry but I know I haven’t processed things fully yet) and when all this is over, on top of all the other bad feelings, I deeply fear the guilt I’ll have for causing her that pain. I truly feel seeing her decline is traumatizing me. If anyone could offer some helpful tips, I would be very grateful.",
+                    style: TextStyle(
+                      fontSize: 14.0, // Adjust the font size as needed
+                      color: Colors.grey, // Adjust the color as needed
+                    ),
+                    maxLines: 3, // Set the maximum number of lines
+                    overflow: TextOverflow.ellipsis, // Add overflow ellipsis
+                  ),
+      ],
+    ),
+  ),
+],
+
+              ),
             ],
           ),
         ],
